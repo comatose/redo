84620e2084a16b585658cd947dbda1ac7b68a6aa
Boncheol Gu <boncheol.gu@gmail.com>
2016-01-30 11:29:02 +0900

    Fixed Setup.hs to create symbolic links properly.

--- Setup.hs
+++ Setup.hs
@@ -1,31 +1,31 @@
 {-# LANGUAGE CPP #-}
 
-import Control.Monad (unless)
+import Control.Monad (forM_, when)
 import Distribution.Simple
 import Distribution.Simple.InstallDirs (InstallDirs(..), fromPathTemplate, toPathTemplate)
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
 import Distribution.Simple.Setup (ConfigFlags(..), fromFlag, fromFlagOrDefault)
-import System.Directory (copyFile, doesFileExist)
 import System.FilePath ((</>))
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+import System.Directory (copyFile)
 #else
+import System.Directory (doesFileExist, removeFile)
 import System.Posix.Files (createSymbolicLink)
 #endif
 
-main = defaultMainWithHooks simpleUserHooks {postInst = postInstall}
-
-postInstall _ _ _ buildInfo = do
-  -- I tried the following seemingly more correct way but received the error:
-  -- "internal error InstallDirs.libsubdir". For now I gave up on tracking it
-  -- down and switched to the following code. If you know what I'm doing wrong
-  -- here, please let me know.
-  -- let installDirs = absoluteInstallDirs pkgDesc buildInfo NoCopyDest
-  let dirs = configInstallDirs $ configFlags buildInfo
-      dir = (fromPathTemplate . fromFlag $ prefix dirs) </> (fromPathTemplate . fromFlagOrDefault (toPathTemplate "bin") $ bindir dirs)
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks {
+  -- Uses a postCopy hook because postInst is not working.
+  postCopy = postInstall}
+ where postInstall _ _ _ buildInfo = do
+         let dirs = configInstallDirs $ configFlags buildInfo
+             dir = (fromPathTemplate . fromFlag $ prefix dirs) </> (fromPathTemplate . fromFlagOrDefault (toPathTemplate "bin") $ bindir dirs)
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
-  copyFile (dir </> "redo.exe") (dir </> "redo-ifchange.exe")
+         copyFile "redo.exe" (dir </> "redo-ifchange.exe")
 #else
-  let symlink = (dir </> "redo-ifchange")
-  exists <- doesFileExist symlink
-  unless exists $ createSymbolicLink (dir </> "redo") symlink
+         let symlinks = map (dir </>) ["redo-ifchange", "redo-ifcreate"]
+         forM_ symlinks $ \symlink -> do
+           exists <- doesFileExist symlink
+           when exists $ removeFile symlink
+           createSymbolicLink "redo" symlink
 #endif
